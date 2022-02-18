c65d26a64ce9aa6cc3558e5c1bff6f6267af97bd
huangys <372117154@qq.com>
2022-02-18 11:31:01 +0800

    初次提交

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'virtual:windi.css'
+import router from '@/router/index';
+import store from '@/store';
+
+
+createApp(App).mount('#app')
---
+++ src/router/route.async.ts
@@ -0,0 +1,12 @@
+// 需要鉴权的业务路由
+import { RouteRecordRaw } from 'vue-router';
+
+const asyncRoutes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    meta: {},
+    redirect: '/home',
+  }
+];
+
+export default asyncRoutes;
